e1691ed83d3666935de9114c1164cee7e5dfb294
Colin Dismuke <colin.dismuke@gmail.com>
2019-02-28 20:01:44 +0100

    Setup Redux folders, store, and middleware

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { BrowserRouter as Router } from "react-router-dom"
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import logger from 'redux-logger'
+import rootReducer from './reducers'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Middleware for error logging and dispatching
+const middleware = applyMiddleware(logger, thunk);
+
+// Create Redux store
+const store = createStore(rootReducer, middleware)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>,
+    document.getElementById('root'));
+
