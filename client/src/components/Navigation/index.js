f4ee54368f40a4eed5e2d2d409a7cb3e9fc77a45
rlmclaughlin <rlmclaughlin@yahoo.com>
2019-03-01 12:35:36 -0600

    added the menu drawer to the navigation bar

--- client/src/components/Navigation/index.js
+++ client/src/components/Navigation/index.js
@@ -6,23 +6,41 @@ const NavContainer = styled.section`
   flex-wrap: wrap; 
   align-items: center; 
   justify-content: space-between;  
-  padding: 0% 7%;
+  padding: 0% 5%;
   background-color: rgba(0, 0, 0, 0.1); 
 `
 const NavbarRight = styled.div`
   font-weight: 600; 
+  font-size: 18px;
+`
+
+const NavbarLeft = styled.div`
+    display: flex; 
+    flex-wrap: wrap; 
+    align-items: center; 
+    width: 60%; 
+`
+
+const MenuDrawer = styled.div`
+    font-size: 24px; 
+    margin-right: 5%; 
 `
 
 class Navigation extends React.Component {
     constructor(){
         super();
     }
-    
+
     render(){
         return(
             <div>
                 <NavContainer>
-                    <h2>Pick Em</h2>
+                    <NavbarLeft>
+                        <MenuDrawer>
+                        <i class="fas fa-bars"></i>
+                         </MenuDrawer>
+                        <h2>Pick Em</h2>
+                    </NavbarLeft>
                     <NavbarRight>
                         <p>Sign In</p> 
                     </NavbarRight>
