22804622bca7469009a78c22758e8c71b20bcdb8
rlmclaughlin <rlmclaughlin@yahoo.com>
2019-03-01 00:52:04 -0600

    Created boilerplate for landing.js file

---
+++ client/src/components/Landing/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default class Landing extends React.Comonent {
+    constructor(){
+        super();
+    }
+    render(){
+        return(
+            <div>
+               
+            </div>
+        )
+    }
+}
