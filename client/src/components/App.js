4cd73278c4cdabb2a2ff4f15e701f7d72a37dfa6
Colin Dismuke <colin.dismuke@gmail.com>
2019-03-01 10:39:43 +0100

    Built and styled Dashboard component, normalized App component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,25 +1,14 @@
 import React, { Component } from 'react';
-import logo from '../assets/logo.svg';
 import './App.css';
+
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <CssBaseline />
+        <h1>Pickem</h1>
       </div>
     );
   }
