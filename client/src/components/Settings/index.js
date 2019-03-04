a6683e05dcf0a57cc55c89b5af7964078376bedd
Bruce Cabanayan <bcabanayan@Unknown.domain>
2019-03-03 17:43:51 -0700

    Added styling for settings elements using withStyles material-ui component

--- client/src/components/Settings/index.js
+++ client/src/components/Settings/index.js
@@ -1,4 +1,18 @@
 import React from 'react'
+import { withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField'; 
+
+const styles = theme => ({
+    container: {
+        display: 'flex',
+        flexWrap: 'wrap'
+    },
+    textField : {
+        marginLeft: theme.spacing.unit,
+        marginRight: theme.spacing.unit,
+        width: 200
+    }
+})
 
 class Settings extends React.Component {
     constructor(){
@@ -6,13 +20,15 @@ class Settings extends React.Component {
     }
     
     render(){
+        const { classes } = this.props;
+
         return(
-            <div>
+            <form className={classes.container}>
                 Test
-            </div>
+            </form>
         )
     }
 }
 
 
-export default Settings;
+export default withStyles(styles)(Settings);
