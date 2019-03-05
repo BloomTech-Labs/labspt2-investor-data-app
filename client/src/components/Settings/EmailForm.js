198f02793d29c37b08fabbeada5946baead7d210
Bruce Cabanayan <bcabanayan@Bruces-MacBook-Pro.local>
2019-03-04 20:06:30 -0700

    Added commenting and cleaned up email form component

--- client/src/components/Settings/EmailForm.js
+++ client/src/components/Settings/EmailForm.js
@@ -1,13 +1,9 @@
 import React from 'react';
 
 // MATERIAL UI COMPONENTS
-import CssBaseline from '@material-ui/core/CssBaseline';
 import { withStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField'; 
 import Button from '@material-ui/core/Button';
-import FormGroup from '@material-ui/core/FormGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Switch from '@material-ui/core/Switch';
 
 // WITHSTYLES
 import styles from './styles';
@@ -15,45 +11,43 @@ import styles from './styles';
 class EmailForm extends React.Component {
     constructor(){
         super();
-        this.state = {
-            // email: 'test@example.com',
-            // newEmail: ''
-        };
     };
 
     render(){
+
+
         const { classes } = this.props;
 
         return(
-            <div>
-                {/* FORM TO UPDATE EMAIL ADDRESS */}
-                <form 
-                    className={classes.container}
-                    id='emailForm'
+            <form 
+                className={classes.container}
+                id='emailForm'
+            >
+                <div>
+                    {/* Show current email address */}
+                    <h3 className={classes.currentHeader}>Current email address:</h3>
+                    <p className={classes.currentValue}>{this.state.email}</p>
+                </div>
+                {/* Text field for new email address */}
+                <TextField 
+                    id='newEmail'
+                    label='New email'
+                    className={classes.textField}
+                    value={this.state.newEmail}
+                    onChange={this.props.handleChange('newEmail')}
+                    margin='normal'
+                />
+                {/* Button to submit new email address */}
+                <Button 
+                    variant='contained' 
+                    color='primary'
+                    className={classes.button}
+                    type='submit'
+                    form='emailForm'
                 >
-                    <div>
-                        <h3 className={classes.currentHeader}>Current email address:</h3>
-                        <p className={classes.currentValue}>{this.state.email}</p>
-                    </div>
-                    <TextField 
-                        id='newEmail'
-                        label='New email'
-                        className={classes.textField}
-                        value={this.state.newEmail}
-                        onChange={this.props.handleChange('newEmail')}
-                        margin='normal'
-                    />
-                    <Button 
-                        variant='contained' 
-                        color='primary'
-                        className={classes.button}
-                        type='submit'
-                        form='emailForm'
-                    >
-                        Update email
-                    </Button>
-                </form>
-            </div>
+                    Update email
+                </Button>
+            </form>
         )
     };
 };
