c72a0f1f247eae8db826a4458cbefa3eda3219a8
iepoch <steaclem@gmail.com>
2019-03-04 22:03:58 -0700

    Made all material-ui imports into 1 import

--- client/src/components/Navigation/SigninModal.js
+++ client/src/components/Navigation/SigninModal.js
@@ -1,13 +1,15 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
 // import TextField from "@material-ui/core/TextField";
-import Dialog from "@material-ui/core/Dialog";
-import DialogActions from "@material-ui/core/DialogActions";
-import DialogContent from "@material-ui/core/DialogContent";
-import DialogContentText from "@material-ui/core/DialogContentText";
-import DialogTitle from "@material-ui/core/DialogTitle";
 import { SingInModal } from "../Styles/Navigation/Signin";
-import { TextField } from "@material-ui/core";
+import {
+  TextField,
+  DialogContent,
+  Dialog,
+  DialogActions,
+  DialogContentText,
+  DialogTitle
+} from "@material-ui/core";
 
 /* TODO:
  * Create a connection to database
