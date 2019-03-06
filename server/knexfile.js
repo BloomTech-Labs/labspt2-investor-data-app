6c697c8f0634e0f97ed49576bccf694d45806964
iepoch <steaclem@gmail.com>
2019-03-05 21:17:37 -0700

    - Added .env file for postgre - Added Server Response for display - Added production  knex setup for pg - Installed libaries pg, cross-env -

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,5 +1,8 @@
 // Update with your config settings.
 
+//Must require the dotENV file to connect with postgre
+require('dotenv').config('/.env');
+
 module.exports = {
 
   development: {
@@ -27,6 +30,20 @@ module.exports = {
     seeds: {
       directory: './data/seeds',
     },
-  }
+  },
+  production: {
+    client: 'pg',
+    connection: {
+      filename: process.env.DATABASE_URL,
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations',
+      tableName: 'knex_migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
+  },
   
 };
