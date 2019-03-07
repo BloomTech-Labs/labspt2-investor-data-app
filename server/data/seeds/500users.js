958ea325e8532d514aa01af600bfbb4742496605
Bruce Cabanayan <bcabanayan@Bruces-MacBook-Pro.local>
2019-03-06 19:51:27 -0700

    Added faker function to create a fake user

--- server/data/seeds/500users.js
+++ server/data/seeds/500users.js
@@ -1,3 +1,13 @@
+const faker = require('faker');
+
+// Faker function to create a fake user based on schema in migration file
+
+const createFakeUser = () => ({
+  firstName: faker.name.firstName(),
+  lastName: faker.name.lastName(),
+  email: faker.internet.email(),
+  password: faker.internet.password()
+});
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
