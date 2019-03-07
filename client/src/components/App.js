78f97aac6f7ce6d127e31e39fac749349f0bbf5c
Rob Salzberg <robsalzberg@gmail.com>
2019-03-06 22:23:45 -0500

    Setup billing page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Landing from '../components/Landing/index'
+import Billing from '../components/Billing/index'
 
 import './App.css';
 
@@ -8,6 +9,7 @@ class App extends Component {
     return (
       <div>
         <Landing />
+        <Billing />
       </div>
     );
   }
