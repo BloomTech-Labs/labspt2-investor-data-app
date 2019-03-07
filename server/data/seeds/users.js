a52289d5496943b76cf9080bf12aa34a25a49107
Bruce Cabanayan <bcabanayan@Bruces-MacBook-Pro.local>
2019-03-06 20:53:31 -0700

    Removed old users seed function and added loop to seed staging sql database with 500 dummy users

--- server/data/seeds/users.js
+++ server/data/seeds/users.js
@@ -1,13 +1,21 @@
+const faker = require('faker');
 
-exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('users').del()
-    .then(function () {
-      // Inserts seed entries  
-      return knex('users').insert([
-        {id: 1, firstName: 'John', lastName: 'Doe', email: 'test@here.com', password: '12345' },
-        {id: 2, firstName: 'Betty', lastName: 'Rubble', email: 'test2@here.com', password: '12345' },
-        {id: 3, firstName: 'Barney', lastName: 'Rubble', email: 'test3@here.com', password: '6789' },
-      ]);
-    });
+// Faker function to create a fake user based on schema in migration file
+
+const createFakeUser = () => ({
+  firstName: faker.name.firstName(),
+  lastName: faker.name.lastName(),
+  email: faker.internet.email(),
+  password: faker.internet.password()
+});
+
+// For loop that runs createFakeUser function to seed database with 500 dummy users
+
+exports.seed = async function(knex) {
+  const fakeUsers = [];
+  const desiredFakeUsers = 100;
+  for (let i = 0; i < desiredFakeUsers; i++) {
+    fakeUsers.push(createFakeUser());
+  };
+  await knex('users').insert(fakeUsers);
 };
