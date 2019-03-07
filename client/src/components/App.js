453679f022d2537e884aeac8cbee5bf955145245
Colin Dismuke <colin.dismuke@gmail.com>
2019-03-07 05:41:49 +0100

    Added withRouter, Switch, and Route to App

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router';
+import { Switch, Route } from 'react-router-dom';
+
 import Landing from '../components/Landing/index'
 import './App.css';
 
