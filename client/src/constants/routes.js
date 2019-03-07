7a8475a3f9488dcf2ee6cbf401ee977f091cf16d
Colin Dismuke <colin.dismuke@gmail.com>
2019-03-07 05:11:14 +0100

    Added routes constants

---
+++ client/src/constants/routes.js
@@ -0,0 +1,6 @@
+export const LANDING = '/'
+export const DASHBOARD = '/dashboard'
+export const REPORTS = '/reports'
+export const TARGETS = '/targets'
+export const SETTINGS = '/settings'
+export const BILLING = '/billing'
