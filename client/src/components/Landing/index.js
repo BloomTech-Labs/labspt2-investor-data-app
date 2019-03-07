45f27d4bb1c44aee19754edde47994fad86b858d
iepoch <steaclem@gmail.com>
2019-03-06 21:17:57 -0700

    - Added the Team List and bare bones view no styles - Added a axios to get all users at this current time. - Added DevList to display users currently to reach MVP

--- client/src/components/Landing/index.js
+++ client/src/components/Landing/index.js
@@ -1,15 +1,34 @@
 import React from "react";
 import Navigation from "../Navigation/index";
-
+import axios from "axios";
+import { Route } from "react-router-dom";
+import DevList from "../Team/DevList";
 class Landing extends React.Component {
-  // constructor(){
-  //     super();
-  // }
-
+  constructor() {
+    super();
+    this.state = {
+      users: []
+    };
+  }
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/api/users")
+      .then(response => {
+        this.setState({ users: response.data });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
   render() {
     return (
       <div>
         <Navigation />
+        <Route
+          exact
+          path="/"
+          render={props => <DevList {...props} users={this.state.users} />}
+        />
       </div>
     );
   }
