da7fd8be27dc02db3054801eb370fab15836cb8e
Colin Dismuke <colin.dismuke@gmail.com>
2019-03-07 05:36:14 +0100

    Removed targets route from route.js

--- client/src/constants/routes.js
+++ client/src/constants/routes.js
@@ -1,6 +1,5 @@
 export const LANDING = '/'
 export const DASHBOARD = '/dashboard'
 export const REPORTS = '/reports'
-export const TARGETS = '/targets'
 export const SETTINGS = '/settings'
 export const BILLING = '/billing'
