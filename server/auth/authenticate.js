2cb165ec8bbb3d72605040924acde0154d23b134
iepoch <steaclem@gmail.com>
2019-03-07 17:46:46 -0700

    Fixed misspelling verfy to verify.

--- server/auth/authenticate.js
+++ server/auth/authenticate.js
@@ -16,7 +16,7 @@ function authenticate (req, res, next)  {
     const token = req.get("Authorization")
 
     if(token) {
-        jwt.verfy(token, jwtKey, (err, decoded) => {
+        jwt.verify(token, jwtKey, (err, decoded) => {
             if(err) return res.status(401).json(err)
             req.decoded = decoded 
             next()
