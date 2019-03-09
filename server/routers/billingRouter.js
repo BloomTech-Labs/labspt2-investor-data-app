b1fd6df7fa4c9b7e8a25d60266d23c9de29d66de
Colin Dismuke <colin.dismuke@gmail.com>
2019-03-09 11:45:34 +0100

    Added example Stripe POST request to billingRouter

--- server/routers/billingRouter.js
+++ server/routers/billingRouter.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const stripe = require('./constants/stripe')
 const billing = require('../data/helpers/billingModel')
+
+// Send a response to the frontend
+const postStripeCharge = res => (stripeErr, stripeRes) => {
+    if (stripeErr) {
+      res.status(500).send({ error: stripeErr });
+    } else {
+      res.status(200).send({ success: stripeRes });
+    }
+};
 
 /************************************ BILLING SECTION ***********************************/
 
@@ -15,6 +25,17 @@ router.get('/', async (req, res) => {
                 .status(500)
                 .json({ error: "The billing could not be retrieved." });
         });
+});
+
+// Example POST request with Stripe
+// TODO: Create actual POST request
+router.post("/", (req, res) => {
+    const body = {
+      source: req.body.token.id,
+      amount: req.body.amount,
+      currency: "usd"
+    };
+    stripe.charges.create(body, postStripeCharge(res));
 });
 
 module.exports = router;
