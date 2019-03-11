ff25ac4947def45fa97b744c76b82b34f58e1b5f
Bruce Cabanayan <bcabanayan@gmail.com>
2019-03-10 18:29:06 -0700

    Wrote reducer to store firstName in state as placeholder for user settings

---
+++ client/src/components/Settings/reducers.js
@@ -0,0 +1,20 @@
+import { FETCHING, GET_SETTINGS, ERROR } from './actions.js';
+
+const initialState = {
+    fetchingSettings: false,
+    error: null,
+    username: ''
+};
+
+const reducer = (state = initialState, action) => {
+    switch(action.type){
+        case FETCHING:
+            return Object.assign({}, state, {fetchingSettings: true});
+        case GET_SETTINGS:
+            return Object.assign({}, state, {fetchingSettings: false, error: '', username: action.payload.username});
+        case ERROR:
+            return Object.assign({}, state, {fetchingSmurfs: false, error: action.error});
+    }
+};
+
+export default reducer;
