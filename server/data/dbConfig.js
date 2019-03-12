d7a10582513044a8f79d8c7be60975d99613c7a5
iepoch <steaclem@gmail.com>
2019-03-11 18:52:11 -0700

    fixing connection issues

--- server/data/dbConfig.js
+++ server/data/dbConfig.js
@@ -1,6 +1,6 @@
 const knex = require('knex');
 const config = require('../knexfile.js');
 
-const dbEnv = process.env.DATABASE_URL || 'staging';
+const dbEnv = process.env.DB_ENV ||'production';
 
 module.exports = knex(config[dbEnv]);
