e388d9a58828eb059afb9678629772b1dd1536e0
rlmclaughlin <rlmclaughlin@yahoo.com>
2019-03-11 20:33:35 -0500

    slightly refactored code so a table could be created through a loop - added css animation for table

--- client/src/components/Landing/index.js
+++ client/src/components/Landing/index.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import Navigation from '../Navigation/index'
-import axios from 'axios'
 import StockTicker from '../StockTicker/index.js'
-import TickerBoard from '../StockTicker/tickerBoard'
 
 
 class Landing extends React.Component {
--- client/src/components/StockTicker/index.js
+++ client/src/components/StockTicker/index.js
@@ -1,62 +1,110 @@
-import React from 'react';
-import TickerBoard from './tickerBoard'
+import React from 'react'
 import axios from 'axios'
+import './tickerBoard.css'
 
 class StockTicker extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-         stocks: [],
-         arr: []
+      timeStamp: {},
+      companies: ['DJI', 'NDAQ', 'SPX', 'AAPL', 'AMZN'], // stock company symbols
+      stocks: [],
     }
   }
 
   componentDidMount(){
-    this.fetchStocks()
+    let promises = this.state.companies.map(company =>   // map that sends array of stock companies through axios
+      axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${company}&interval=5min&apikey=MRYZL6KHH9MMJYIF`));
+    this.fetchStocks(promises)
   }
 
-  fetchStocks = () => {
-    const item = 'AMZN'
-    axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${item}&interval=5min&apikey=MRYZL6KHH9MMJYIF`)
-      .then( response => {
-         this.setState({
-           stocks: response.data['Time Series (5min)']
-         })
-         console.log(this.state.stocks)
+  fetchStocks = (promises) => {  // Receives array of companies and returns values of the stock symbols from the api 
+    let stocks = []
+    let timeStamp
+    axios
+      .all(promises)
+      .then(results => {
+        results.forEach(result => {  // loops through keys to access targeted values of stock(s)
+        
+          if (result.data.Note) {
+            throw new Error()
+          }
+
+          let data = result.data['Time Series (5min)']
+          let timeStamps = Object.keys(data)
+          let current = data[timeStamps[0]]
+          timeStamp = timeStamps[0]
+
+          stocks.push({
+            company: result.data['Meta Data']['2. Symbol'], 
+            values: current
+          })
+        });
+
+        this.setState({
+          stocks,
+          timeStamp
+        });
       })
-      .catch( error => { console.log ( "we've encountered an error")})
+      .catch(error => {
+        console.error('There was an error with the network requests', error)
+      });
   }
 
-  //changeObj = (obj) => {
-//
-  //  const result = Object.keys(obj).map(function(key) {
-  //    return [key, obj[key]];
-  //  });
-  //  
-  //  let temp = result[0];  //this turns the big object into an array
-  //  let a = temp[1];    // the second item of the array has the four values we need
-  //  let b = a[ '1. open' ];   //just grabbing the first value, since the key has spaces we
-  //  console.log("1. open:", b)      // have to use bracket notation
-  //  let c = a[ '2. high' ];
-  //  console.log("2. high:", c);
-  //}
- 
-   
-  render() { 
-    const timestamp = Object.keys(this.state.stocks)[0]
-    let cube = this.state.stocks[timestamp]
-  
-    setTimeout(() => {console.log(cube['1. open'])}, 6000)
-    //this.changeObj(this.state.stocks)
-    
+  change = (close, start) => {  // function for calculating the change of a stocks gain/loss by %
+    let deduct = close - start
+    let divide = deduct / start
+    let solution = divide * 100
+      return solution.toFixed(3)
+  }
+
+  render() {
+    if(!this.state.stocks.length) {  // returns loading sign while data is being retrieved from API
+      return <div>Loading...</div>
+    }
+
+    let rows = [];
+
+    const open = '1. open'
+    const high = '2. high'
+    const low = '3. low'
+    const close = '4. close'
+
+    this.state.stocks.forEach( (stock, index) => {  // loops through array of stock values and creates a table
+      console.log(stock)
+      rows.push(
+        <tr>
+          <td>{ stock.company }</td>
+          <td>{ stock.values[open] }</td>
+          <td>{ stock.values[high] }</td>
+          <td>{ stock.values[low] }</td>
+          <td>{ stock.values[close] }</td>
+          <td>{ `${this.change(stock.values[close], stock.values[open])}%` }</td>
+        </tr>
+      )
+    });
+
     return (
       <div>  
-        <div> 
-          
-          </div> 
-        <TickerBoard  />
+        <div className='table'>
+        <table className="container">
+	        <thead>
+		        <tr>
+			        <th><h1>Company</h1></th>
+			        <th><h1>Start</h1></th>
+			        <th><h1>High</h1></th>
+			        <th><h1>Low</h1></th>
+              <th><h1>Close</h1></th>
+              <th><h1>Change</h1></th> 
+		        </tr>
+	        </thead>
+	        <tbody>
+		        { rows }  
+	        </tbody>
+        </table>
+        </div> 
       </div> 
-    );
+    )
   }
 }
 
