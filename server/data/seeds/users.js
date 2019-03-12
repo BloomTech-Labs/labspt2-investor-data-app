3e660eb6348ac09b17903a1d517bf4f66f07ecbc
iepoch <steaclem@gmail.com>
2019-03-11 19:44:23 -0700

    changing faker for a moment

--- server/data/seeds/users.js
+++ server/data/seeds/users.js
@@ -1,4 +1,4 @@
-const faker = require('faker');
+const faker = require("faker");
 
 // Faker function to create a fake user based on schema in migration file
 
@@ -12,15 +12,15 @@ const createFakeUser = () => ({
 // Deletes the existing users seeds, then runs the for loop that runs createFakeUser function to seed database with 500 dummy users
 
 exports.seed = function(knex, Promise) {
-  const desiredFakeUsers = 100;
-  return knex('users').del().truncate()
-    .then(async function () {
-      for (let i = 0; i < 5; i ++) {
-        for (let i = 0; i < desiredFakeUsers; i++) {
-          const fakeUsers = [];
-          fakeUsers.push(createFakeUser());
-          await knex('users').insert(fakeUsers);
-        };
-      };
+  const desiredFakeUsers = 500;
+  return knex("users")
+    .del()
+    .truncate()
+    .then(async function() {
+      for (let i = 0; i < desiredFakeUsers; i++) {
+        const fakeUsers = [];
+        fakeUsers.push(createFakeUser());
+        await knex("users").insert(fakeUsers);
+      }
     });
 };
