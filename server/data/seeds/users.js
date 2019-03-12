e4d1dd4f3c061c073314b58c4afb5eee98a919b6
iepoch <steaclem@gmail.com>
2019-03-11 19:47:00 -0700

    added the faker set to 100 back

--- server/data/seeds/users.js
+++ server/data/seeds/users.js
@@ -12,15 +12,17 @@ const createFakeUser = () => ({
 // Deletes the existing users seeds, then runs the for loop that runs createFakeUser function to seed database with 500 dummy users
 
 exports.seed = function(knex, Promise) {
-  const desiredFakeUsers = 500;
+  const desiredFakeUsers = 100;
   return knex("users")
     .del()
     .truncate()
     .then(async function() {
-      for (let i = 0; i < desiredFakeUsers; i++) {
-        const fakeUsers = [];
-        fakeUsers.push(createFakeUser());
-        await knex("users").insert(fakeUsers);
+      for (let i = 0; i < 5; i++) {
+        for (let i = 0; i < desiredFakeUsers; i++) {
+          const fakeUsers = [];
+          fakeUsers.push(createFakeUser());
+          await knex("users").insert(fakeUsers);
+        }
       }
     });
 };
