0b2209bffaa1080f784233c43191c7371d2677d5
Colin Dismuke <colin.dismuke@gmail.com>
2019-03-13 21:00:01 +0100

    Added framework for AppBar

--- client/src/components/Navigation/index.js
+++ client/src/components/Navigation/index.js
@@ -1,36 +1,56 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
 import {
-  NavContainer,
-  MenuDrawer,
-  NavbarLeft,
-  NavbarRight
-} from "../Styles/Navigation/Main";
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  IconButton
+} from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
 import SigninModal from "./SigninModal";
 import SignupModal from "./SignupModal";
 
-class Navigation extends React.Component {
-  //   constructor() {
-  //     super();
-  //   }
-
-  render() {
-    return (
-      <div>
-        <NavContainer>
-          <NavbarLeft>
-            <MenuDrawer>
-              <i className="fas fa-bars" />
-            </MenuDrawer>
-            <h2>Pick Em</h2>
-          </NavbarLeft>
-          <NavbarRight>
-            <SigninModal />
-            <SignupModal />
-          </NavbarRight>
-        </NavContainer>
-      </div>
-    );
+const styles = {
+  root: {
+    flexGrow: 1
+  },
+  grow: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20
   }
-}
+};
 
-export default Navigation;
+const Navigation = props => {
+  const { classes } = props;
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="Menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" color="inherit" className={classes.grow}>
+            News
+          </Typography>
+          <SigninModal />
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+
+Navigation.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Navigation);
