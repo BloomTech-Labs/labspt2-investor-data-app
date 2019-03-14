37ee257b03ac39260dfbca5c721b9fbcc7889457
Rob Salzberg <robsalzberg@gmail.com>
2019-03-13 22:43:05 -0400

    Added register form inside register tab

--- client/src/components/Navigation/RegisterLogin.js
+++ client/src/components/Navigation/RegisterLogin.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
+import React from "react";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Typography from "@material-ui/core/Typography";
 
 function TabContainer(props) {
   return (
@@ -22,21 +22,28 @@ function TabContainer(props) {
 }
 
 TabContainer.propTypes = {
-  children: PropTypes.node.isRequired,
+  children: PropTypes.node.isRequired
 };
 
 const styles = theme => ({
   root: {
     flexGrow: 1,
-    backgroundColor: theme.palette.background.paper,
-  },
+    backgroundColor: theme.palette.background.paper
+  }
 });
 
 export default class RegisterLogin extends React.Component {
-  state = {
-    open: false,
-    value: 0
-  };
+  constructor() {
+    super();
+    this.state = {
+      open: false,
+      value: 0,
+      email: "",
+      passwordMessage: "",
+      password1: "",
+      password2: ""
+    };
+  }
 
   handleClickOpen = () => {
     this.setState({ open: true });
@@ -48,13 +55,36 @@ export default class RegisterLogin extends React.Component {
   handleChange = (event, value) => {
     this.setState({ value });
   };
+  inputchange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  submit = event => {
+    event.preventDefault();
+    const { email, password1, password2 } = this.state;
+    if (password1 !== password2) {
+      this.setState({
+        passwordMessage: "Passwords do not match!"
+      });
+    } else {
+      this.setState({
+        passwordMessage: "Passwords match!"
+      });
+    }
+  };
 
   render() {
-    const { classes } = this.props;
-    const { value } = this.state;
+    //const { classes } = this.props;
+    const { password2, password1, passwordMessage, value } = this.state;
     return (
       <div>
-        <Button variant="outlined" color="primary" onClick={this.handleClickOpen}>
+        <Button
+          variant="outlined"
+          color="primary"
+          onClick={this.handleClickOpen}
+        >
           Get Started
         </Button>
         <Dialog
@@ -70,7 +100,39 @@ export default class RegisterLogin extends React.Component {
                 <Tab label="Login" />
               </Tabs>
             </AppBar>
-            {value === 0 && <TabContainer>Register</TabContainer>}
+            {value === 0 && (
+              <TabContainer>
+                <form onSubmit={this.submit}>
+                  Enter Email Address:
+                  <input
+                    type="email"
+                    name="email"
+                    value={this.state.email}
+                    onChange={this.inputchange}
+                  />
+                  <br />
+                  Enter Password:
+                  <input
+                    type="password"
+                    name="password1"
+                    value={this.state.password1}
+                    onChange={this.inputchange}
+                  />
+                  {passwordMessage ? <span>{passwordMessage}</span> : null}
+                  <br />
+                  Confirm Password:
+                  <input
+                    type="password"
+                    label="Confirm Passowrd"
+                    name="password2"
+                    value={this.state.password2}
+                    onChange={this.inputchange}
+                  />
+                  {passwordMessage ? <span>{passwordMessage}</span> : null}
+                  <button type="submit">Submit</button>
+                </form>
+              </TabContainer>
+            )}
             {value === 1 && <TabContainer>Login</TabContainer>}
           </DialogContent>
         </Dialog>
@@ -78,6 +140,6 @@ export default class RegisterLogin extends React.Component {
     );
   }
 }
-RegisterLogin.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
+//RegisterLogin.propTypes = {
+//  classes: PropTypes.object.isRequired,
+//};
