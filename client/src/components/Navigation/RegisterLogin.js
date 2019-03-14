3648e633a21644e98bb0721d2ab1f1168cb401be
Rob Salzberg <robsalzberg@gmail.com>
2019-03-13 22:04:47 -0400

    Nested two tabs inside modal

--- client/src/components/Navigation/RegisterLogin.js
+++ client/src/components/Navigation/RegisterLogin.js
@@ -6,10 +6,36 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+
+function TabContainer(props) {
+  return (
+    <Typography component="div" style={{ padding: 8 * 3 }}>
+      {props.children}
+    </Typography>
+  );
+}
+
+TabContainer.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+  },
+});
 
 export default class RegisterLogin extends React.Component {
   state = {
     open: false,
+    value: 0
   };
 
   handleClickOpen = () => {
@@ -19,8 +45,13 @@ export default class RegisterLogin extends React.Component {
   handleClose = () => {
     this.setState({ open: false });
   };
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
 
   render() {
+    const { classes } = this.props;
+    const { value } = this.state;
     return (
       <div>
         <Button variant="outlined" color="primary" onClick={this.handleClickOpen}>
@@ -33,10 +64,20 @@ export default class RegisterLogin extends React.Component {
         >
           <DialogTitle id="form-dialog-title">Register or Login</DialogTitle>
           <DialogContent>
-            
+            <AppBar position="static">
+              <Tabs value={value} onChange={this.handleChange}>
+                <Tab label="Register" />
+                <Tab label="Login" />
+              </Tabs>
+            </AppBar>
+            {value === 0 && <TabContainer>Register</TabContainer>}
+            {value === 1 && <TabContainer>Login</TabContainer>}
           </DialogContent>
         </Dialog>
       </div>
     );
   }
 }
+RegisterLogin.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
