e77a5b9e38fe94c929283a897515f8c404501e82
Colin Dismuke <colin.dismuke@gmail.com>
2019-03-15 18:34:02 +0100

    Added base styles to Navigation

--- client/src/components/Navigation/index.js
+++ client/src/components/Navigation/index.js
@@ -1,6 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
+
+// Material UI
 import { withStyles } from "@material-ui/core/styles";
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+
 import {
   NavContainer,
   MenuDrawer,
@@ -10,6 +20,19 @@ import {
 //import SigninModal from "./SigninModal";
 //import SignupModal from "./SignupModal";
 import RegisterLogin from "./RegisterLogin";
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
 
 class Navigation extends React.Component {
   //   constructor() {
