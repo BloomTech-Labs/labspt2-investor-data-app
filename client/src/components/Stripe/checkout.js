79f66567151ae9b588c2c166b117e1efaef596ac
Rob Salzberg <robsalzberg@gmail.com>
2019-03-18 22:39:26 -0400

    Added Stripe checkout component

---
+++ client/src/components/Stripe/checkout.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import axios from 'axios';
+import StripeCheckout from 'react-stripe-checkout';
+
+import STRIPE_PUBLISHABLE from './constants/stripe';
+import PAYMENT_SERVER_URL from './constants/server';
+
+const CURRENCY = 'EUR';
+
+const fromUSDToCent = amount => amount * 100;
+
+const successPayment = data => {
+  alert('Payment Successful');
+};
+
+const errorPayment = data => {
+  alert('Payment Error');
+};
+
+const onToken = (amount, description) => token =>
+  axios.post(PAYMENT_SERVER_URL,
+    {
+      description,
+      source: token.id,
+      currency: CURRENCY,
+      amount: fromUSDSToCent(amount)
+    })
+    .then(successPayment)
+    .catch(errorPayment);
+
+const Checkout = ({ name, description, amount }) =>
+  <StripeCheckout
+    name={name}
+    description={description}
+    amount={fromUSDToCent(amount)}
+    token={onToken(amount, description)}
+    currency={CURRENCY}
+    stripeKey={STRIPE_PUBLISHABLE}
+  />
+
+export default Checkout;
