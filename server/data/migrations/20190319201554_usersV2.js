8fcd17146727b8444ab08a5dd750b18c00dd5e81
bcabanayan <bcabanayan@gmail.com>
2019-03-19 20:48:43 -0700

    Updated users V2 migration file to add columns for user's selection to receive texts and emails

--- server/data/migrations/20190319201554_usersV2.js
+++ server/data/migrations/20190319201554_usersV2.js
@@ -1,8 +1,33 @@
 
-exports.up = function(knex, Promise) {
-  
+exports.up = function (knex, Promise) {
+    return knex.schema.createTable('users', users => {
+        users.increments();
+        users
+            .string('firstName', 128)
+            .notNullable();
+        users
+            .string('lastName', 128)
+            .notNullable();
+        users
+            .string('username', 128)
+            .notNullable()
+            .unique();
+        users
+            .string('email', 128)
+            .notNullable()
+            .unique();
+        users
+            .string('password', 128)
+            .notNullable();
+        users
+            .boolean('receiveEmails')
+            .defaultTo(false);
+        users
+            .boolean('receiveTexts')
+            .defaultTo(false);
+    });
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = function (knex, Promise) {
+    return knex.schema.dropTableIfExists('users');
 };
