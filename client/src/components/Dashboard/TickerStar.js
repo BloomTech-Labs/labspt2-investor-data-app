b5c2b3adaa4fae20675e490eea61d963f02d06b6
rlmclaughlin <rlmclaughlin@yahoo.com>
2019-03-19 21:09:56 -0500

    updates

--- client/src/components/Dashboard/TickerStar.js
+++ client/src/components/Dashboard/TickerStar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import axios from 'axios'
 
 class TickerStar extends React.Component{
     constructor(props){
@@ -15,7 +16,21 @@ class TickerStar extends React.Component{
             this.setState({
                 selected: true,
                 star: 'fa fa-star'
-            })       
+            })   
+            const newSymbol = {
+                symbol: this.props.id,
+                target: 1
+
+            }
+
+
+                   axios.post('http://www.localhost:5000/api/favorites', newSymbol)
+                     .then( response => {
+                         this.setState({
+                             newSymbol: { symbol: '', targets: null, users_id: null}
+                         })
+                     })
+                     .catch( err => { console.log( "we've encountered an error")})
             console.log(this.props.id)
          } else {
              this.setState({
