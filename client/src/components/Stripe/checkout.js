246dd6522379a5320f954680b280953b13daae08
Rob Salzberg <robsalzberg@gmail.com>
2019-03-19 22:22:21 -0400

    Added Stripe checkout to billing page

--- client/src/components/Stripe/checkout.js
+++ client/src/components/Stripe/checkout.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import axios from 'axios';
 import StripeCheckout from 'react-stripe-checkout';
+import PAYMENT_SERVER_URL from '../../constants/payment_server';
+import STRIPE_PUBLISHABLE from '../../constants/publisher_key';
 
-import STRIPE_PUBLISHABLE from './constants/stripe';
-import PAYMENT_SERVER_URL from './constants/server';
 
 const CURRENCY = 'USD';
 
@@ -17,23 +17,21 @@ const errorPayment = data => {
   alert('Payment Error');
 };
 
-const onToken = (amount, description) => token =>
+const onToken = (amount) => token =>
   axios.post(PAYMENT_SERVER_URL,
     {
-      description,
       source: token.id,
       currency: CURRENCY,
-      amount: fromUSDSToCent(amount)
+      amount: fromUSDToCent(amount)
     })
     .then(successPayment)
     .catch(errorPayment);
 
-const Checkout = ({ name, description, amount }) =>
+const Checkout = ({ name, amount }) =>
   <StripeCheckout
     name={name}
-    description={description}
     amount={fromUSDToCent(amount)}
-    token={onToken(amount, description)}
+    token={onToken(amount)}
     currency={CURRENCY}
     stripeKey={STRIPE_PUBLISHABLE}
   />
