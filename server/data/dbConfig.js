957438cd5bdb33c20bf70130274df08639db4bde
bcabanayan <bcabanayan@gmail.com>
2019-03-21 18:07:42 -0700

    Restored db environment to production

--- server/data/dbConfig.js
+++ server/data/dbConfig.js
@@ -1,6 +1,6 @@
 const knex = require("knex");
 const config = require("../knexfile.js");
 
-const dbEnv = process.env.DB_ENV || "staging";
+const dbEnv = process.env.DB_ENV || "production";
 
 module.exports = knex(config[dbEnv]);
