c44e66f47099cca894887aad00ab6008c7b20154
iepoch <steaclem@gmail.com>
2019-03-30 10:02:21 -0700

    fixed getById to get by id instead of UID

--- server/data/helpers/usersModel.js
+++ server/data/helpers/usersModel.js
@@ -7,10 +7,10 @@ module.exports = {
     return db("users");
   },
 
-  getById: uid => {
+  getById: id => {
     let query = db("users");
-    if (uid) {
-      query.where("users.uid", uid).first();
+    if (id) {
+      query.where("users.id", id).first();
       return query;
     }
     return db("users");
