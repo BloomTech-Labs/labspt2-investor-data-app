3a0bebcab145eb63dcd871c640b047c8e2ad7afd
iepoch <steaclem@gmail.com>
2019-04-03 19:42:56 -0700

    Removed Pickem from who uses. Added application

--- client/src/components/Landing/index.js
+++ client/src/components/Landing/index.js
@@ -42,7 +42,7 @@ const Landing = () => {
 
       <div className="steps landing__section">
         <div className="container">
-          <h2>Who uses PICKEM?</h2>
+          <h2>Who uses our application?</h2>
         </div>
         <div className="container">
           <div className="steps__inner">
