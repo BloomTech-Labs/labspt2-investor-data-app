a787d9ffbc0c10ce30ed8eab4b896f99c74c2f40
Willie Yturralde <willieino@hotmail.com>
2019-04-06 21:13:53 -0700

    clean up code

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -84,8 +84,7 @@ class App extends Component {
       this.state.currentEmail,
       this.state.userUID
       );
-      //const endpoint = "https://pickemm.herokuapp.com/api/users";
-      const endpoint = "http://localhost:5000/api/users";
+      const endpoint = "https://pickemm.herokuapp.com/api/users";
     axios
       .post(endpoint, creds)
       .then(res => {
