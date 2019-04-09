27308325ee29b7fe5aebce2a4ec6e8f83d07209c
Rob Salzberg <robsalzberg@gmail.com>
2019-04-08 22:10:56 -0400

    Fix merge issues

--- client/src/components/Stripe/checkout.js
+++ client/src/components/Stripe/checkout.js
@@ -1,20 +1,20 @@
-import React, {Component} from 'react';
-import axios from 'axios';
-import StripeCheckout from 'react-stripe-checkout';
-import STRIPE_PUBLISHABLE from '../../constants/stripe';
-import PAYMENT_SERVER_URL from '../../constants/server';
-import {fire} from '../Auth/firebaseConfig';
+import React, { Component } from "react";
+import axios from "axios";
+import StripeCheckout from "react-stripe-checkout";
+import STRIPE_PUBLISHABLE from "../../constants/stripe";
+import PAYMENT_SERVER_URL from "../../constants/server";
+import { fire } from "../Auth/firebaseConfig";
 
-const CURRENCY = 'USD';
+const CURRENCY = "USD";
 
 const fromUSDToCent = amount => amount * 100;
 
 const successPayment = data => {
-  alert('Payment Successful');
+  alert("Payment Successful");
 };
 
 const errorPayment = data => {
-  alert('Payment Error');
+  alert("Payment Error");
 };
 
 class Checkout extends Component {
@@ -22,7 +22,7 @@ class Checkout extends Component {
     super(props);
     this.state = {
       accountType: null,
-      usersId: fire.currentUser.uid,
+      usersId: fire.currentUser.uid
     };
   }
 
@@ -32,11 +32,11 @@ class Checkout extends Component {
 
   typeAcct = amount => {
     if (Number(amount) === 5) {
-      this.setState({accountType: 1});
+      this.setState({ accountType: 1 });
     } else if (Number(amount) === 15) {
-      this.setState({accountType: 2});
+      this.setState({ accountType: 2 });
     } else if (Number(amount) === 30) {
-      this.setState({accountType: 3});
+      this.setState({ accountType: 3 });
     }
   };
 
@@ -46,13 +46,13 @@ class Checkout extends Component {
         .post(PAYMENT_SERVER_URL, {
           source: token.id,
           currency: CURRENCY,
-          amount: fromUSDToCent(this.props.amount),
+          amount: fromUSDToCent(this.props.amount)
         })
         .then(successPayment => {
           if (successPayment) {
             const bill = this.state;
-            console.log('OnToken:', bill);
-            const endpoint = 'https://pickemm.herokuapp.com/api/billing';
+            console.log("OnToken:", bill);
+            const endpoint = "https://pickemm.herokuapp.com/api/billing";
             axios
               .post(endpoint, bill)
               .then(successPayment)
