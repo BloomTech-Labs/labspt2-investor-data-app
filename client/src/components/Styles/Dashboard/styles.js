b85433195628da99d181ed9ec19cb0da920c62b9
Willie Yturralde <willieino@hotmail.com>
2019-04-12 15:54:36 -0700

    add tooltips

--- client/src/components/Styles/Dashboard/styles.js
+++ client/src/components/Styles/Dashboard/styles.js
@@ -6,14 +6,15 @@ const styles = theme => ({
     //height: "100vh"
   },
   grid: {
-    width: 1200,
+   // width: 1200,
+    width: 800,
     margin: `0 ${theme.spacing.unit * 2}px`,
     [theme.breakpoints.down("sm")]: {
       width: "calc(100% - 20px)"
     }
   },
   paper: {
-    padding: theme.spacing.unit * 3,
+    padding: theme.spacing.unit * 2,
     textAlign: "left",
     color: theme.palette.text.primary,
     // height: '40vh',
