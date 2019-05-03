c051470545484e73f3d8aad471a90ed1d24b3e2b
Rob Salzberg <robsalzberg@gmail.com>
2019-05-02 22:17:35 -0400

    Change striperouter to old style

--- server/routers/stripeRouter.js
+++ server/routers/stripeRouter.js
@@ -3,19 +3,31 @@ const router = express.Router ();
 const stripe = require ('../constants/stripe');
 
 router.post ('/', (req, res) => {
-  try {
   stripe.customers.create ({
     source: req.body.source,
     email: req.body.email,
-  });
+  },
   
-  res.json({status});
- } catch (err) {
-    res.status(500).end();
-  }
-   stripe.subscriptions.create ({
+  function(err, customer
+    
+    ) {
+      console.log("customer", customer);
+    if (err) {
+      res.send({
+        success: false,
+        message: "Error"
+      });
+    } else { 
+      res.send ({
+        success: true,
+        message: "Success",
+      }) 
+    } 
+    stripe.subscriptions.create ({
    customer: customer.id,
    plan: req.body.plan
   }); 
-},)
+})
+});
+
 module.exports = router;
