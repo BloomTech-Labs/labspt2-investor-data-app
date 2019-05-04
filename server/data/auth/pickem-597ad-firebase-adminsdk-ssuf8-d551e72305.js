3aceba8fc9797a276ce49aa4953f99e9d9198140
iepoch <steaclem@gmail.com>
2019-05-04 08:22:59 -0700

    Not sure why the Private_Key.replace was missing added it back

--- server/data/auth/pickem-597ad-firebase-adminsdk-ssuf8-d551e72305.js
+++ server/data/auth/pickem-597ad-firebase-adminsdk-ssuf8-d551e72305.js
@@ -2,7 +2,7 @@ module.exports = {
   type: process.env.REACT_APP_TYPE,
   project_id: process.env.REACT_APP_PROJECT_ID,
   private_key_id: process.env.REACT_APP_PRIVATE_ID,
-  private_key: process.env.REACT_APP_PRIVATE_KEY,
+  private_key: process.env.REACT_APP_PRIVATE_KEY.replace(/\\n/g, "\n"),
   client_email: process.env.REACT_APP_CLIENT_EMAIL,
   client_id: process.env.REACT_APP_CLIENT_ID,
   auth_uri: process.env.REACT_APP_AUTH_URI,
