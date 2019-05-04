45e554612b0d88320abab52a3298c8114e1d90e8
iepoch <steaclem@gmail.com>
2019-05-04 12:12:16 -0700

    Backend fix

--- server/data/auth/pickem-597ad-firebase-adminsdk-ssuf8-d551e72305.js
+++ server/data/auth/pickem-597ad-firebase-adminsdk-ssuf8-d551e72305.js
@@ -1,12 +1,12 @@
 module.exports = {
   type: process.env.REACT_APP_TYPE,
-  project_id: process.env.REACT_APP_PROJECT_ID,
-  private_key_id: process.env.REACT_APP_PRIVATE_ID,
-  private_key: process.env.REACT_APP_PRIVATE_KEY.replace(/\\n/g, "\n"),
-  client_email: process.env.REACT_APP_CLIENT_EMAIL,
-  client_id: process.env.REACT_APP_CLIENT_ID,
-  auth_uri: process.env.REACT_APP_AUTH_URI,
-  token_uri: process.env.REACT_APP_TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.REACT_APP_AUTH_PROVIDER,
-  client_x509_cert_url: process.env.REACT_APP_CLIENT_URL
+  project_id: process.env.PROJECT_ID,
+  private_key_id: process.env.PRIVATE_ID,
+  private_key: process.env.PRIVATE_KEY.replace(/\\n/g, "\n"),
+  client_email: process.env.CLIENT_EMAIL,
+  client_id: process.env.CLIENT_ID,
+  auth_uri: process.env.AUTH_URI,
+  token_uri: process.env.TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.AUTH_PROVIDER,
+  client_x509_cert_url: process.env.CLIENT_URL
 };
