e77b811510999ee6316c7c4cf226633d2befde4e
iepoch <steaclem@gmail.com>
2019-05-12 13:39:17 -0700

    Fixed the issue of rendering from Reports to Settings. Possibly bandaid but works. added toast to the entire app and on click commands to the settings so it notifies the user

--- client/src/components/Settings/OptEmailsTextsForm.js
+++ client/src/components/Settings/OptEmailsTextsForm.js
@@ -4,7 +4,7 @@ import { fire } from "../Auth/firebaseConfig";
 // Redux imports
 import { connect } from "react-redux";
 import { getSettings, updateSettings } from "../../actions/settingsActions.js";
-
+import { withToastManager } from "react-toast-notifications";
 // Material UI Components
 import { withStyles } from "@material-ui/core/styles";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
@@ -28,7 +28,7 @@ class OptEmailsTextsForm extends React.Component {
 
   render() {
     const { classes } = this.props;
-
+    const { toastManager } = this.props;
     return (
       <div className={classes.emailTextContainer}>
         <Typography variant="h6">Email and text preferences</Typography>
@@ -37,8 +37,18 @@ class OptEmailsTextsForm extends React.Component {
           <FormControlLabel
             control={
               <Switch
-                checked={this.props.settings.receiveEmails}
+                checked={this.props.settings.receiveEmails ? true : false}
                 onChange={this.handleSwitch("receiveEmails")}
+                onClick={() => {
+                  toastManager.add(
+                    "Activate emails to receive information on your favorite stocks email.",
+                    {
+                      appearance: "info",
+                      autoDismiss: true,
+                      pauseOnHover: true
+                    }
+                  );
+                }}
                 value="receiveEmails"
                 color="secondary"
               />
@@ -50,8 +60,18 @@ class OptEmailsTextsForm extends React.Component {
           <FormControlLabel
             control={
               <Switch
-                checked={this.props.settings.receiveTexts}
+                checked={this.props.settings.receiveTexts ? true : false}
                 onChange={this.handleSwitch("receiveTexts")}
+                onClick={() => {
+                  toastManager.add(
+                    "To activate the stocks scanner add your phone number and then enable texts alerts",
+                    {
+                      appearance: "info",
+                      autoDismiss: true,
+                      pauseOnHover: true
+                    }
+                  );
+                }}
                 value="receiveTexts"
                 color="secondary"
               />
@@ -79,9 +99,11 @@ const mapDispatchToProps = dispatch => ({
     dispatch(updateSettings(uid, updatedCommPreference))
 });
 
-export default withStyles(styles)(
-  connect(
-    mapStateToProps,
-    mapDispatchToProps
-  )(OptEmailsTextsForm)
+export default withToastManager(
+  withStyles(styles)(
+    connect(
+      mapStateToProps,
+      mapDispatchToProps
+    )(OptEmailsTextsForm)
+  )
 );
