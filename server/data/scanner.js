c8bd24a7975c86d5d7d33599803207f1f2690a82
Willie Yturralde <willieino@hotmail.com>
2019-05-14 12:32:48 -0700

    repair code

--- server/data/scanner.js
+++ server/data/scanner.js
@@ -3,9 +3,10 @@ const nexmo = require("./nexmoConfig");
 
 
 scanner = () => {
+
   const YOUR_VIRTUAL_NUMBER = "18572560178";
   let userNumber = "";
-  // const URL = "http://localhost:5000/api/sms"; // ********** CHANGE FOR DEPLOYMENT *************
+ // const URL = "http://localhost:5000/api/sms"; // ********** CHANGE FOR DEPLOYMENT *************
   const URL = "https://pickemm.herokuapp.com/api/sms";
 
   getCustomers = () => {
@@ -15,9 +16,11 @@ scanner = () => {
       .then(response => {
         for (let i = 0; i < response.data.length; i++) {  // ***************** USE FOR DEPLOYMENT ************
           // Step through the user data
+         console.log("response.data:", response.data[i].usersId)
           customers.push(response.data[i].usersId);
         }
         // Send users to the next subroutine
+      
         getUsers(customers);
       })
       .catch(err => {
@@ -25,26 +28,35 @@ scanner = () => {
       });
   };
 
-  getUsers = customers =>
+  getUsers = customers => {
+    console.log("made it to getUsers2")
     axios
       .get(`${URL}/users`) // Get User Data
       .then(response => {
         // for (let i = 0; i < response.data.length; i++) {  // ***************** USE FOR DEPLOYMENT ************
         for (let i = 0; i < customers.length; i++) {
+          console.log("i:", i)
           // Step through the user data
           for (let j = 0; j < response.data.length; j++) {
+            console.log("j:", j)
             // Check if user is a match to billing usersId
+            console.log("response.data[j].uid:", response.data[j].uid)
+            console.log("customers[i]:", customers[i])
             if (response.data[j].uid === customers[i]) {
               // Check if receiveTexts is enabled
+              console.log("response.data[i].receiveTexts:", response.data[i].receiveTexts)
               if (response.data[j].receiveTexts === true) {
                 // Format the users number
+                console.log("response.data[j].phoneNumber:", response.data[j].phoneNumber)
+                //+1 (606) 671-0901
                 userNumber =
-                  "1" +
-                  response.data[j].phoneNumber.slice(1, 1) +
+                "1" +  
+                response.data[j].phoneNumber.slice(1, 1) +
                   response.data[j].phoneNumber.slice(4, 7) +
                   response.data[j].phoneNumber.slice(9, 12) +
                   response.data[j].phoneNumber.slice(-4); // Save the users phone number               
-                getFavorites(response.data[j].uid); // Send each user to the next subroutine
+               console.log("userNumber:", userNumber)
+                  getFavorites(response.data[j].uid); // Send each user to the next subroutine
               }
             }
           }
@@ -53,99 +65,98 @@ scanner = () => {
       .catch(err => {
         console.log("There was an error accessing the users table", err);
       });
-};
+  };
 
-getFavorites = uid => {
-  // Get all the favorites for each user
-  let companies = [];
-  axios
-    .get(`${URL}/favorites`) // User favorites
-    .then(response => {
-      for (let i = 0; i < response.data.length; i++) {
-        // Step through the favorites data
-        if (response.data[i].uid === uid) {
-          // Check for a match to the user uid
-          companies.push(response.data[i].symbol); // Add each symbol to companies array
+  getFavorites = uid => {
+    // Get all the favorites for each user
+    console.log("made it to getfavorites")
+    let companies = [];
+    axios
+      .get(`${URL}/favorites`) // User favorites
+      .then(response => {
+        for (let i = 0; i < response.data.length; i++) {
+          // Step through the favorites data
+          if (response.data[i].uid === uid) {
+            // Check for a match to the user uid
+            companies.push(response.data[i].symbol); // Add each symbol to companies array
+          }
         }
-      }
-      getStocks(companies); // Send the companies to next subroutine
-    })
-    .catch(err => {
-      console.log("There was an error");
-    });
-};
+        getStocks(companies); // Send the companies to next subroutine
+      })
+      .catch(err => {
+        console.log("There was an error");
+      });
+  };
 
-getStocks = companies => {
-  console.log("made it to getstocks")
-  let promises = companies.map((company) =>
-    axios.get(`https://www.alphavantage.co/query?function=MACD&symbol=${company}&interval=daily&series_type=open&apikey=TFUONSVQ3ZDFXFPG`));
+  getStocks = companies => {
+    console.log("made it to getstocks")
+    let promises = companies.map((company) =>
+      axios.get(`https://www.alphavantage.co/query?function=MACD&symbol=${company}&interval=daily&series_type=open&apikey=TFUONSVQ3ZDFXFPG`));
 
-  let timeStamp;
-  let x = false;
-  let y = false;
+    let timeStamp;
+    let x = false;
+    let y = false;
 
-  axios
-    .all(promises)
-    .then(results => {
+    axios
+      .all(promises)
+      .then(results => {
 
-      results.forEach(result => {
-        // Pulling the company symbol out of the url
-        let symbol = result.config.url.slice(55, 62)
-        let newSymbol = symbol.substr(0, symbol.indexOf("&"))
-        let data = result.data["Technical Analysis: MACD"]; // Accesses correct object within API
-        let timeStamps = Object.keys(data);
-        x = false;
-        y = false;
-
-        // Subtract current Signal from current MACD Determine if it is a positive or negative value
-        if (data[timeStamps[0]].MACD - data[timeStamps[0]].MACD_Signal > 0) {
-          x = true;
-        }
-
-        // Subtract yesterday's Signal from yesterday's MACD  Determine if it is a positive or negative value
-        // CHANGE THE 1'S TO 20'S FOR TESTING TO GIVE IT A LARGER RANGE or uncomment the next line
-        if (data[timeStamps[1]].MACD - data[timeStamps[1]].MACD_Signal > 0) {
-          //if (data[timeStamps[20]].MACD - data[timeStamps[20]].MACD_Signal > 0) { 
-          y = true;
-        }
-
-        // If the 2 values aren't equal then the lines crossed, If they are both true or both false the lines did not cross
-        if (!x === y) {
+        results.forEach(result => {
+          // Pulling the company symbol out of the url
+          let symbol = result.config.url.slice(55, 62)
+          let newSymbol = symbol.substr(0, symbol.indexOf("&"))
+          let data = result.data["Technical Analysis: MACD"]; // Accesses correct object within API
+          let timeStamps = Object.keys(data);
           x = false;
           y = false;
-          console.log("the lines have crossed");
-          // Construct user text message
-          let message = `The MACD Signal lines for ${newSymbol} have crossed.`
-          console.log("message:", message);
-          console.log("phoneNumber:", userNumber);
-          console.log("*******************************");
-          // Run the Nexmo api
-          nexmo.message.sendSms(
-            YOUR_VIRTUAL_NUMBER,
-            userNumber,
-            message,
-            (err, responseData) => {
-              if (err) {
-                console.log(err);
-              } else {
-                console.dir(responseData);
-                // need to add code to check the response in order to make the app slow down
-                // when the data speed exceeds acceptable values
+
+          // Subtract current Signal from current MACD Determine if it is a positive or negative value
+          if (data[timeStamps[0]].MACD - data[timeStamps[0]].MACD_Signal > 0) {
+            x = true;
+          }
+
+          // Subtract yesterday's Signal from yesterday's MACD  Determine if it is a positive or negative value
+          // CHANGE THE 1'S TO 20'S FOR TESTING TO GIVE IT A LARGER RANGE or uncomment the next line
+          if (data[timeStamps[1]].MACD - data[timeStamps[1]].MACD_Signal > 0) {
+            //if (data[timeStamps[20]].MACD - data[timeStamps[20]].MACD_Signal > 0) { 
+            y = true;
+          }
+
+          // If the 2 values aren't equal then the lines crossed, If they are both true or both false the lines did not cross
+          if (!x === y) {
+            x = false;
+            y = false;
+            console.log("the lines have crossed");
+            // Construct user text message
+            let message = `The MACD Signal lines for ${newSymbol} have crossed.`
+            console.log("message:", message);
+            console.log("phoneNumber:", userNumber);
+            console.log("*******************************");
+            // Run the Nexmo api
+            nexmo.message.sendSms(
+              YOUR_VIRTUAL_NUMBER,
+              userNumber,            
+              message,
+              (err, responseData) => {
+                if (err) {
+                  console.log(err);
+                } else {
+                  console.dir(responseData);
+                }
               }
-            }
-          );
-        } else {
-          console.log("the lines have not crossed");
-        }
+            );
+          } else {
+            console.log("the lines have not crossed");
+          }
+        });
+      })
+      .catch(error => {
+        console.error("There was an error with the network requests", error);
       });
-    })
-    .catch(error => {
-      console.error("There was an error with the network requests", error);
-    });
-  //})
-};
-getCustomers();
-console.log("Scan complete");
+    //})
+  };
+  getCustomers();
+  console.log("Scan complete");
 
 };
 
